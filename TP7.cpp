17cd39c30a1d1d321ba8c8d6a7be5912f5ef7a3a
JuHolland <33092664+JuHolland@users.noreply.github.com>
2018-04-22 21:10:01 +0200

    Add files via upload

--- TP7.cpp
+++ TP7.cpp
@@ -5,15 +5,20 @@ using namespace std;
 int main() {
     //Test Hello World!
 	cout<<"Hello World!\0"<<endl;
+
+
 	//création d'un tableau de caractères
-    char test[3]={'a','b','\0'};
-    cout<<test<<endl;
-    //test du constructeur à partir d'un char*
+  char test[3]={'a','b','\0'};
+  cout<<test<<endl;
+
+  //test du constructeur à partir d'un char*
 	String s1(test);
 	cout<<s1.str()<<endl;
+
 	//taille de la chaine de caractère s1
 	cout<<s1.length()<<endl;
 	cout<<s1.max_size()<<endl;
+
 	//test resize (int n)
 	s1.resize(23);
 	//taille mémoire allouée pour s1
@@ -22,6 +27,7 @@ int main() {
 	char test1[6]={'a','l','i','c','e','\0'};
 	//création d'une string à partir de ce tableau de caractères
 	String s2(test1);
+
 	//test resize (int n)
 	s2.resize(4);
 	cout<<s2.str()<<endl;
@@ -36,6 +42,7 @@ int main() {
 	s3.resize(6);
 	cout<<s3.str()<<endl;
 	cout<<s3.capacity()<<endl;
+
 	//tests de resize (int n, char c)
 	s3.resize(10,'Y');
 	cout<<s3.str()<<endl;
@@ -46,24 +53,23 @@ int main() {
 	s3.resize(3,'R');
 	cout<<s3.str()<<endl;
 	cout<<s3.capacity()<<endl;
+
 	//test opérateur = pour un char
 	char test3[6]={'H','e','l','l','o','\0'};
 	String str1(test3);
-    str1 = 'x';
-    cout<<str1.str()<<endl;
-    cout<<str1.length()<<endl;
-    cout<<str1.capacity()<<endl;
+  str1 = 'x';
+  cout<<str1.str()<<endl;
+  cout<<str1.length()<<endl;
+  cout<<str1.capacity()<<endl;
     
-    //test opérateur + pour un char* à droite
-    String str2(test3);
-    char add1[7]={' ','W','o','r','l','d','\0'};
-    char test4[7]={'a','\0'};
-    String s4(test4);
-    s4 = str2 + add1;
-    cout<<s4.str()<<endl;
+  //test opérateur + pour un char* à droite
+  String str2(test3);
+  char add1[7]={' ','W','o','r','l','d','\0'};
+  char test4[7]={'a','\0'};
+  String s4(test4);
+  s4 = str2 + add1;
+  cout<<s4.str()<<endl;
 
-
-  cout<<"Tests de Juliette"<<endl;
 
   //test constructeur par copie
   String s5(s1); //construteur par copie
@@ -79,7 +85,6 @@ int main() {
 	cout<<s6.size()<<endl;
   cout<<s6.size()<<endl;
 
-
   //test clear
   s5.clear();
   cout<<s5.str()<<endl;
@@ -92,6 +97,55 @@ int main() {
   char c = 's';
   String s8 = s7+c;
   cout<<s8.str()<<endl;
+
+
+  //test empty
+  char test11[9]={'p','a','s',' ','v','i','d','e','\0'};
+  String s11(test11);
+  cout<<s11.str()<<endl;
+  if(s11.empty()){
+    cout<<"string vide \n";
+  }
+  else {
+    cout<<"string pas vide \n";
+  }
+
+    char test12[1]={'\0'};
+    String s12(test12);
+    cout<<s12.str()<<endl;
+    if(s12.empty()){
+      cout<<"string vide \n";
+    }
+    else {
+      cout<<"string pas vide \n";
+    }
+
+   //test reserve
+    char test13[17]={'p','a','s',' ','a','s','s','e','z',' ','d','e',' ','p','l','a','\0'};  
+    String s13(test13);
+    cout<<s13.str()<<endl;
+         cout<<s13.capacity()<<endl;
+    s13.reserve(19);
+    cout<<s13.str()<<endl; //modifie pas le contenu du string
+         cout<<s13.capacity()<<endl;   
+
+   //test operateur =(char*)
+   char egale[7]={'G','o','o','d','b','y','e'};
+        char test14[6]={'H','e','l','l','o','\0'};
+        String str14(test14);
+   cout<<str14.str()<<endl;
+   str14 = egale;
+   cout<<str14.str()<<endl;
+
+   //test operateur +(String)
+   char cat[4]={'c','a','t','\0'};
+   char dog[4]={'d','o','g','\0'};
+        String str15(cat);
+        String str16(dog);
+   String str17 = str15 + str16;
+   cout<<str17.str()<<endl;
+   cout<<str17.length()<<endl;
+   cout<<str17.capacity()<<endl;
 
 	return 0;
 } 
